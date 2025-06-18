166d45f866cd270da1cc361c16301f6d40a89362
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2025-06-18 15:48:46 -0400

    feat: add IconButton component

---
+++ src/components/Buttons/IconButton.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+  defineProps<{
+    ariaLabel?: string;
+    onClick?: () => void;
+  }>();
+</script>
+
+<template>
+  <button
+    :aria-label="ariaLabel"
+    @click="onClick"
+    class="w-[30px] h-[30px] bg-gray-200 dark:bg-slate-700 rounded-full p-1 hover:bg-gray-300 dark:hover:bg-slate-600 flex items-center justify-center"
+  >
+    <slot />
+  </button>
+</template>
