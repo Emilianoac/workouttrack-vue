959d5169a5de1deede6839ce307579bc98326834
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2025-06-18 15:49:36 -0400

    feat: add TheToggle Component.

---
+++ src/components/Shared/ThemeToggle.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+  import { useDark, useToggle } from "@vueuse/core";
+  import IconMoon from "@/components/Icons/Moon.vue";
+  import IconSun from "@/components/Icons/Sun.vue";
+  import IconButton from "@/components/Buttons/IconButton.vue";
+
+  const isDark = useDark();
+  const toggle = useToggle(isDark);
+</script>
+
+<template>
+  <IconButton @click="toggle()" aria-label="Cambiar tema">
+    <IconSun v-if="isDark" />
+    <IconMoon v-else />
+  </IconButton>
+</template>
+
+<style lang="postcss" scoped></style>
