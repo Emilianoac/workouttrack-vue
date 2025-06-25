26262eeef6433e3fd61836863bef885d2deb48af
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2025-06-25 17:57:31 -0400

    style(DashboardLayout): adjust padding for main content area in responsive layout

--- src/layouts/DashboardLayout.vue
+++ src/layouts/DashboardLayout.vue
@@ -8,7 +8,7 @@
   <div class="block lg:grid lg:grid-cols-[230px_1fr] grid-rows-[auto_1fr] md:overflow-hidden h-screen">
     <MainHeader class="col-span-2" />
     <Sidebar class="hidden lg:block" />
-    <main class="p-7 overflow-auto">
+    <main class="p-7 overflow-auto pb-20 lg:pb-7">
       <router-view />
     </main>
   </div>
