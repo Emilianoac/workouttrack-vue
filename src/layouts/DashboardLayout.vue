778764f9120254498ce5e8a98abd720cf5912a7b
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2025-06-24 22:31:31 -0400

    style(DashboardLayout): update grid classes for responsive layout adjustments

--- src/layouts/DashboardLayout.vue
+++ src/layouts/DashboardLayout.vue
@@ -5,14 +5,14 @@
 </script>
 
 <template>
-  <div class="block md:grid md:grid-cols-[230px_1fr] grid-rows-[auto_1fr] md:overflow-hidden h-screen">
+  <div class="block lg:grid lg:grid-cols-[230px_1fr] grid-rows-[auto_1fr] md:overflow-hidden h-screen">
     <MainHeader class="col-span-2" />
-    <Sidebar class="hidden md:block" />
+    <Sidebar class="hidden lg:block" />
     <main class="px-5 md:px-10 py-10 overflow-auto">
       <router-view />
     </main>
   </div>
-  <BottomNavigation class="md:hidden" />
+  <BottomNavigation class="lg:hidden" />
 </template>
 
 <style lang="postcss"></style>
