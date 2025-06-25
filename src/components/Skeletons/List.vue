a0b1b67236d2b3d2e7d66bf37661faa92a37f42b
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2025-06-24 22:20:59 -0400

    feat(Skeleton): create ListSkeleton component and adjust props for Skeleton

---
+++ src/components/Skeletons/List.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+  import Skeleton from "@/components/Skeletons/Skeleton.vue";
+
+  defineOptions({
+    name: "ListSkeleton",
+  });
+
+  const { items = 5, itemHeight = "h-16" } = defineProps<{
+    items?: number;
+    itemHeight?: string;
+  }>();
+</script>
+
+<template>
+  <Skeleton v-for="i in items" :key="i" class="w-full mb-4 last:mb-0" :style="{ height: itemHeight }" />
+</template>
+
+<style lang="postcss" scoped></style>
--- src/components/Skeletons/Skeleton.vue
+++ src/components/Skeletons/Skeleton.vue
@@ -4,18 +4,18 @@
   });
 
   defineProps<{
-    width: string;
-    height: string;
+    width?: string;
+    height?: string;
   }>();
 </script>
 
 <template>
-  <div :style="{ width: width, height: height }" class="skeleton"></div>
+  <div :style="{ width, height }" class="skeleton"></div>
 </template>
 
 <style lang="postcss" scoped>
   .skeleton {
     @apply w-full h-4;
-    @apply animate-pulse bg-gray-300 dark:bg-slate-700 rounded-md;
+    @apply animate-pulse bg-gray-200 dark:bg-slate-700 rounded-md;
   }
 </style>
