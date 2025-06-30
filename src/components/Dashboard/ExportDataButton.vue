3570bd01036f33302b9d3fa1d430c5e32d5511de
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2025-06-29 21:10:37 -0400

    feat(export): refactor export function to use useWorkout composable

--- src/components/Dashboard/ExportDataButton.vue
+++ src/components/Dashboard/ExportDataButton.vue
@@ -1,76 +1,39 @@
 <script lang="ts" setup>
-import { supabase } from '@/lib/supabaseClient'
+  import { useWorkout } from "@/composables/workouts/useWorkout";
+  import DownloadIcon from "@/components/Icons/Download.vue";
 
-async function exportarDesdeSupabase() {
-  try {
-    const { data, error } = await supabase
-      .from('workout_logs')
-      .select(
-        `
-      created_at,
-      notes,
-      routine:routine_id (name),
-      workout_log_entries (
-        target_sets,
-        target_reps,
-        target_duration_sec,
-        target_rest_between_sets_sec,
-        exercise:exercise_id (
-          name
-        ),
-        set_logs (
-          set_number,
-          reps,
-          duration_sec,
-          weight_kg,
-          is_body_weight,
-          rest_between_sets_sec,
-          notes
-        )
-      )
-    `,
-      )
-      .order('created_at', { ascending: false })
+  const { getDownloadableWorkouts } = useWorkout();
 
-    // order set_logs by set_number
-    if (data) {
-      data.forEach((workout) => {
-        workout.workout_log_entries.forEach((entry) => {
-          entry.set_logs.sort((a, b) => a.set_number - b.set_number)
-        })
-      })
+  async function handleWorkoutsDowndload() {
+    try {
+      const data = await getDownloadableWorkouts();
+
+      const blob = new Blob([JSON.stringify(data, null, 2)], { type: "application/json" });
+      const url = URL.createObjectURL(blob);
+      const link = document.createElement("a");
+      link.href = url;
+      link.download = "workouts_logs.json";
+      document.body.appendChild(link);
+      link.click();
+      URL.revokeObjectURL(url);
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log(error.message);
+      } else {
+        console.error("Unknown error occurred while downloading workouts.", error);
+      }
+      return;
     }
-
-    const blob = new Blob([JSON.stringify(data, null, 2)], { type: 'application/json' })
-    const url = URL.createObjectURL(blob)
-    const enlace = document.createElement('a')
-    enlace.href = url
-    enlace.download = 'workouts_logs.json'
-    document.body.appendChild(enlace)
-    enlace.click()
-    URL.revokeObjectURL(url)
-
-    if (error) throw new Error(error.message)
-  } catch (error) {
-    if (error instanceof Error) {
-      console.log(error.message)
-      alert(`Error al obtener los datos: ${error.message}`)
-    } else {
-      console.error('Error desconocido:', error)
-      alert('Error desconocido al obtener los datos.')
-    }
-    return
   }
-}
 </script>
 
 <template>
   <button
-    @click="exportarDesdeSupabase"
+    @click="handleWorkoutsDowndload"
     type="button"
-    class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors"
+    class="app-btn app-btn-primary font-semibold text-sm flex items-center gap-2"
   >
-    Exportar datos en formato JSON
+    Exportar datos <DownloadIcon class="w-5 h-5" />
   </button>
 </template>
 
